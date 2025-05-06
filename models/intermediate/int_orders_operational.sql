8a2e6051dfa2fde78bb40bbf0765e0f5e3f356e4
Laurence MARRON <laurahaumea@gmail.com>
2025-05-06 17:46:22 +0000

    orders_operational : columns added (revenue, quantity,  ship_cost, shipping fee) as required at next step for finance_days

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -2,6 +2,11 @@ SELECT
     sales.orders_id
     ,sales.date_date
     ,(sales.margin + sh.shipping_fee -sh.logcost -sh.ship_cost) AS operational_margin
+    ,sales.revenue                                                      --new for finance_days 
+    ,sales.quantity                                                     --new for finance_days 
+    ,sales.purchase_cost                                                --new for finance_days 
+    ,sh.ship_cost                                                      --new for finance_days
+    ,sh.shipping_fee                                                      --new for finance_days
 FROM  {{ ref("int_orders_margin")}} AS sales
 LEFT JOIN  {{ ref("stg_raw__ship")}} AS sh
     USING (orders_id)

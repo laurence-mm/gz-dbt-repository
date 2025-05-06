e367f6dcc76fd7a860b437f7735cefd492404dbd
Laurence MARRON <laurahaumea@gmail.com>
2025-05-06 18:03:03 +0000

    finance_days built with modified int_orders_operational (columns added)

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -6,8 +6,8 @@ select
  ,ROUND(SUM(operational_margin),2) AS operational_margin
  ,ROUND(SUM(purchase_cost),2) AS purchase_cost
  ,ROUND(SUM(shipping_fee),2) AS shipping_fee
- --,,ROUND(SUM(logcost),2)
- ,ROUND(SUM(quantity),2) AS qty_of_products_sold
+ ,ROUND(SUM(logcost),2) AS log_cost
+ ,SUM(quantity) AS qty_of_products_sold
 FROM {{ref("int_orders_operational")}}
 GROUP BY date_date
 ORDER BY date_date DESC

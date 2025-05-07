2e1ba7947bc4cc324f471642ab4089be192799d0
Laurence MARRON <laurahaumea@gmail.com>
2025-05-07 17:51:34 +0000

    finance_campaigns_month.sql Mart Model created Add marketing campaing data >step5 - aggregated for each month

---
+++ models/mart/finance/finance_campaigns_month.sql
@@ -0,0 +1,18 @@
+SELECT 
+    DATE_TRUNC(date_date, MONTH) AS date_month  --The resulting value is always rounded to the beginning of granularity
+    ,EXTRACT(MONTH FROM date_date) AS month_
+    ,ROUND(SUM(ads_margin),2) AS ads_margin
+    ,ROUND(SUM(average_basket),2) AS average_basket
+    ,ROUND(SUM(operational_margin),2) AS operational_margin
+    ,SUM(ads_cost) AS ads_cost
+    ,SUM(ads_impression) AS ads_impression
+    ,SUM(ads_click) AS ads_click
+    ,SUM(qty_of_products_sold) AS qty_of_products_sold
+    ,ROUND(SUM(revenue),2) AS revenue
+    ,ROUND(SUM(purchase_cost),2) AS purchase_cost
+    ,ROUND(SUM(margin),2) AS margin
+    ,ROUND(SUM(shipping_fee),2) AS shipping_fee
+    ,ROUND(SUM(logcost),2) AS logcost
+    ,ROUND(SUM(ship_cost),2) AS ship_cost
+FROM  {{ ref("finance_campaigns_day")}}
+GROUP by date_month, month_
